e58df6121b45c1970ac6f7223feca628cc1aa041
Liz <stoneliz@hotmail.com>
2017-06-30 01:40:41 +0000

    Connects home.html to the application

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-var http = require("http"); 
-http.createServer(function(req,res) {
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Home page');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
+var http = require('http'),
+ fs = require('fs');
+function serveStaticFile(res, path, contentType, responseCode) {
+ if(!responseCode) responseCode = 200;
+ fs.readFile(__dirname + path, function(err,data) {
+ if(err) {
+ res.writeHead(500, { 'Content-Type': 'text/plain' });
+ res.end('500 - Internal Error');
+ } else {
+ res.writeHead(responseCode,
+ { 'Content-Type': contentType });
+ res.end(data);
+ }
+ });
+}
+http.createServer(function(req,res){
+ // normalize url by removing querystring, optional
+ // trailing slash, and making lowercase
+ var path = req.url.replace(/\/?(?:\?.*)?$/, '')
+ .toLowerCase();
+ switch(path) {
+ case '':
+ serveStaticFile(res, '/public/home.html', 'text/html');
+ break;
+ case '/about':
+ serveStaticFile(res, '/public/about.html', 'text/html');
+ break;
+ case '/img/logo.jpg':
+ serveStaticFile(res, '/public/img/logo.jpg',
+
+ 'image/jpeg');
+ break;
+ default:
+ serveStaticFile(res, '/public/404.html', 'text/html',
+ 404);
+ break;
+ }
 }).listen(process.env.PORT || 3000);
+console.log('Server started; press Ctrl-C to terminate....');
